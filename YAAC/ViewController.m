2a2d6b547517ff25ee8c1f22084ccbfe95b25e8e
Bsetecip10 <bsetecip10@Nirmals-Mac-mini.local>
2014-12-05 16:09:50 +0530

    BlockConceptClearation
    BlockConceptClearation

--- YAAC/ViewController.m
+++ YAAC/ViewController.m
@@ -9,15 +9,112 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    //***************************BLOCK CREATING/DECLARING****************
 
+    int (^add)(int, int, int);
+}
 @end
+
+typedef int(^BlockOperation) (int, int);
+
+BlockOperation operationCreater(int op)
+{
+    if (op == 0)
+        return ^(int a, int b){ int result=0; result = a+b; return result;};
+     if(op == 1)
+         return ^(int a, int b){ int result=0; result = a*b; return result;};
+    if(op == 2)
+        return ^(int a, int b){int result=0;result = a/b;return result;};
+    return 0;
+};
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
+    BlockOperation sum = operationCreater(1);
+    NSLog(@"%d",sum(44,4));
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //***************************BLOCK CALLING****************
+    add=^(int a, int b, int c){
+        int result=0;
+        result = a + b + c;
+        return result;
+    };
+    NSLog(@"%d",add(2,2,2));
+
+    //*****************2222***********************************
+    add=^(int x, int y, int z){
+        int result=0;
+        result=x*y*z;
+        return result;
+    };
+    NSLog(@"%d",add(2,2,2));
+    
+    //**************33333********************************
+    add=^(int x, int y, int z){
+        int result=0;
+        result=x*y;
+        return result;
+    };
+    NSLog(@"%d",add(2,2,7));
+    
+    //******************44444*******************************
+    add=^(int x, int y, int z){
+        int result=0;
+        result=x;
+        return result;
+    };
+    NSLog(@"%d",add(3,2,2));
+
+    //**********BLOCK CALLING***********************************
+    NSLog(@"%d",add1(10,2,5));
+    
+    //***********WE CAN ASSIGN A BLOCK TO AN INTEGER**************
+    int sub=^(int p, int q){
+        int result=0;
+        result = p-q;
+        return result;
+        
+    }(18,4);
+    NSLog(@"%d",sub);
+    
+    //*****************************************
+    void (^hw)(void) = ^{NSLog(@"hello, world!"); };
+    hw();
+    
+    //***********************************************
+    
+    func(hw);
+    
+    //***************************************
+    function(^{
+        NSLog(@"success");
+    });
+    
 }
+
+    int (^add1)(int, int, int)=^(int x, int y, int z){
+        int result=0;
+        result=x/y;
+        return result;
+    };
+
+
+    void func(void (^a) (void)) // we do need to use an identifier in the parameter list now, of course
+    {
+        NSLog(@"going to invoke the passed in block now.");
+        a();
+    };
+
+
+    void function(void(^b)(void))
+    {
+        b();
+    }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
